c322f644a0d8dac91847833019f8ec21e988a6f0
extranodev <jeomade19@gmail.com>
2023-08-25 19:38:11 +0100

    Model validation default and custom message extracting model validiation error messages

---
+++ ModelValidationsExample/ModelValidationsExample/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelValidationsExample.Models;
+
+namespace ModelValidationsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("register")]
+        public IActionResult Index(Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                string errors = string.Join("\n",
+                    ModelState.Values.SelectMany(value => value.Errors).
+                    Select(err => err.ErrorMessage).ToList());
+
+                return BadRequest(errors);
+            }
+            return Content($"{person}");
+        }
+    }
+}
---
+++ ModelValidationsExample/ModelValidationsExample/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidationsExample.Models
+{
+    public class Person
+    {
+        [Required(ErrorMessage ="Person's name cannot be empty")]
+        public string? PersonName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+        public double? Price { get; set; }
+        public override string ToString()
+        {
+            return $"Person Object ----\nPerson name: " +
+                $"{PersonName}\nEmail: {Email}\nPhone: {Phone}\n" +
+                $"Password: {Password}\nConfirm Password: {ConfirmPassword}\n" +
+                $"Price: {Price}";
+        }
+    }
+}
